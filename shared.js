334f94cbfbbe8678670e30648f007d55ad963516
acorreiaa <amandaluana777@gmail.com>
2020-11-12 07:36:47 -0300

    Chore Animation Package Complete

--- shared.js
+++ shared.js
@@ -11,6 +11,7 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var toggleButton = document.querySelector('.toggle-button')
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
@@ -55,6 +56,17 @@ toggleButton.addEventListener('click', function () {
     setTimeout(function() {
         backdrop.classList.add('open');
     }, 10);
+});
+
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('Animation started', event);
 })
 
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('Animation started', event);
+})
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log('Animation started', event);
+});
 
